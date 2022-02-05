6af95f3e648556aa038ffec687a2099272519b43
JulioMontas <hello@juliomontas.com>
2022-02-05 15:30:54 -0500

    formmmm

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,12 +14,42 @@ const ContactPage = () => {
         </div>
 
         <div>
+        {/*
           <form method="post" name="contact-form" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="text" name="name" placeholder="Your Name" />
             <input type="email" name="email" placeholder="name@name.com" />
             <textarea type="text" name="message" />
             <button>Send</button>
           </form>
+          */}
+
+
+          <form name="contact-me" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+
+
+
+
+
         </div>
 
       </Container>
