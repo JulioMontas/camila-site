36c5e0e3f40f40abdca3192184219fd888674edf
JulioMontas <hello@juliomontas.com>
2022-02-05 15:18:34 -0500

    form debugging

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,10 +15,10 @@ const ContactPage = () => {
 
         <div>
           <form method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
-            <label>Email<input type="email" name="email" /></label>
-            <label>Name<input type="text" name="name" /></label>
-            <label>Message<input type="text" name="message" /></label>
-            <button type="submit">Send</button>
+            <input type="text" name="name" placeholder="Your Name" />
+            <input type="email" name="email" placeholder="name@name.com" />
+            <textarea type="text" name="message" />
+            <button>Send</button>
           </form>
         </div>
 
