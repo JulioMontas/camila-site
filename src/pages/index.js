b09b52d351c47e9340bef6d641be7041f90aedd8
JulioMontas <hello@juliomontas.com>
2022-02-05 03:29:06 -0500

    homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,48 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import Container from "../components/container";
 import Header from "../components/header";
 
+
 // markup
-const IndexPage = () => {
+const IndexPage = ({data}) => {
   return (
     <Container>
     <title>Home Page</title>
     <Header />
-    </Container> 
+      {data.slideShow.edges.map(({node}) => (
+        <div key={node.ide}>
+          <GatsbyImage image={node.childImageSharp.gatsbyImageData} alt={node.base} />
+        </div>
+      ))}
+    </Container>
   )
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    slideShow: allFile(
+      filter: {relativeDirectory: {eq: "bg"}}
+      sort: {fields: base, order: ASC}
+    ) {
+      edges {
+        node {
+          id
+          base
+          childImageSharp {
+            gatsbyImageData(
+              aspectRatio: 1.1
+              placeholder: BLURRED
+              quality: 70
+              blurredOptions: {width: 100}
+              layout: FULL_WIDTH
+            )
+          }
+        }
+      }
+    }
+  }
+`
