511a150386cd84f71f10b59521c969c52c2a8045
JulioMontas <hello@juliomontas.com>
2022-02-05 20:00:20 -0500

    formmmmmmmmmmm

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import Layout from "../components/layout";
 import Seo from "../components/seo"
 import Container from "../components/container";
+import DummyForm from "../components/dummyForm"
 
 const ContactPage = () => {
   return (
@@ -12,27 +13,7 @@ const ContactPage = () => {
           <h2>Contact Me</h2>
           <a href="https://www.instagram.com/chameleoncamila/"><b>Instagram:</b>@ChameleonCamila</a>
         </div>
-        <form
-          name="contact-meeee"
-          method="POST"
-          data-netlify-recaptcha="false"
-          data-netlify="true"
-        >
-          <p>
-            <label>
-              E-mail <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message <textarea name="message"></textarea>
-            </label>
-          </p>
-          <div data-netlify-recaptcha="true"></div>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        <DummyForm />
       </Container>
     </Layout>
   )
---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Success = () => {
+  return (
+  <div>
+    <h2>You dropped me a new line</h2>
+    <h4> Congrats,i&#39;ll be sure to check it out when i can and add a reply</h4>
+  </div>
+ )
+}
+export default Success
